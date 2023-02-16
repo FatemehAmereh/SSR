4f0b0b03b113de2c841dafed4d01c79d801c2e9b
Fatemeh RMG <fateme.rmg@gmail.com>
2023-02-16 21:34:12 +0200

    clean up 1

--- SSRFS.fs
+++ SSRFS.fs
@@ -11,13 +11,6 @@ uniform float SCR_HEIGHT;
 uniform mat4 invProjection;
 uniform mat4 projection;
 
-
-float ReturnZInView(vec3 ts){
-	vec4 r = invProjection * vec4(ts*2 - vec3(1,1,1),1);
-	r /= r.w;
-	return r.z;
-}
-
 bool rayIsOutofScreen(vec2 ray){
 	return (ray.x > 1 || ray.y > 1 || ray.x < 0 || ray.y < 0) ? true : false;
 }
@@ -45,7 +38,7 @@ vec3 TraceRay(vec3 rayPos, vec3 dir, int iterationCount){
 }
 
 void main(){
-	float maxRayDistance = 50.0f;
+	float maxRayDistance = 100.0f;
 
 	//View Space ray calculation
 	vec3 pixelPositionTexture;
@@ -77,61 +70,4 @@ void main(){
 
 	vec3 outColor = TraceRay(pixelPositionTexture, rayDirectionTexture, screenSpaceMaxDistance);
 	reflectionColor = vec4(outColor, 1);
-
-	/*
-	vec2 texCoord = vec2(gl_FragCoord.x / SCR_WIDTH,  gl_FragCoord.y / SCR_HEIGHT);		// 0< <1
-	vec3 normalView = texture(gNormal, texCoord).rgb;	
-	float depth = texture(depthMap, texCoord).r;
-	float depthView = depth * 2 - 1;		// -1< <1				
-	vec4 positionView = invProjection * vec4((texCoord * 2) - vec2(1,1), depthView, 1); 
-	positionView /= positionView.w;
-	vec3 reflectionView = normalize(reflect(positionView.xyz, normalView));
-
-	if(reflectionView.z > 0){	//reflection ray going towards the camera
-		refColor = vec4(0,0,0,1);
-		return;
-	}
-
-	vec3 rayEndPositionView = positionView.xyz + reflectionView * maxRayDistance;
-
-	vec3 pixelPositionTexture = vec3(texCoord, depth);
-	
-	vec4 rayEndPositionTexture = projection * vec4(rayEndPositionView,1);
-	rayEndPositionTexture /= rayEndPositionTexture.w;
-	rayEndPositionTexture.xyz = (rayEndPositionTexture.xyz + vec3(1,1,1)) / 2.0f;
-
-	vec3 rayDirectionTexture = rayEndPositionTexture.xyz - pixelPositionTexture;	//dp
-
-	ivec2 screenSpaceStartPosition = ivec2(pixelPositionTexture.x * SCR_WIDTH, pixelPositionTexture.y * SCR_HEIGHT); 
-	ivec2 screenSpaceEndPosition = ivec2(rayEndPositionTexture.x * SCR_WIDTH, rayEndPositionTexture.y * SCR_HEIGHT); 
-
-	ivec2 screenSpaceDistance = screenSpaceEndPosition - screenSpaceStartPosition;
-
-	int screenSpaceMaxDistance = max(abs(screenSpaceDistance.x), abs(screenSpaceDistance.y)) / 2;
-	vec3 dirTexture = rayDirectionTexture / max(screenSpaceMaxDistance, 0.001f);
-
-
-	vec3 rayStart = pixelPositionTexture;
-	float sampleDepth;
-	vec3 reflectionColor = vec3(0.0f,0,0.0f);
-	bool hit = false;
-	for(int i = 0; i < screenSpaceMaxDistance; i++){
-		rayStart += dirTexture;
-		if(rayStart.x > 1 || rayStart.y > 1 || rayStart.x < 0 || rayStart.y < 0){
-			break;
-		}
-		sampleDepth = texture(depthMap, rayStart.xy).r;
-
-		float depthDif = rayStart.z - sampleDepth;
-		if(depthDif >= 0 && depthDif < 0.00001){ //we have a hit
-			//reflectionColor = vec3(0.6f,0,0);
-			hit = true;
-			reflectionColor = texture(colorBuffer, rayStart.xy).rgb;
-			break;
-		}
-	}
-	
-	refColor = vec4(reflectionColor,1);
-	*/
-	//refColor = hit ? vec4(reflectionColor,1) : vec4(skyColor,1);
 }
